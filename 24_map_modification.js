6abd15c869bc9b295f5e7fe48f0b651519e3cd05
sandrine <sandrinezocca@gmail.com>
2019-05-21 15:22:39 +0200

    les modifications

--- 24_map_modification.js
+++ 24_map_modification.js
@@ -1,5 +1,6 @@
 /*
 VIII.3. LA MODIFICATION
+
 De même que pour les interactions précédentes,
 la modification de données vectorielles est réalisée en ajoutant
 l'interaction ol.interaction.Modify à l'objet Map.
@@ -9,39 +10,41 @@ et les conditions de modifications des sommets.
 Le code suivant montre la mise en place d'une modification simple.
 */
 
-// classes nécéssaires pour afficher la carte
-import 'ol/ol.css';
-import Map from 'ol/Map';
-import View from 'ol/View';
-
-// classes pour les vecteurs
-import VectorLayer from 'ol/layer/Vector';
-import VectorSource from 'ol/source/Vector';
-import GeoJSON from 'ol/format/GeoJSON';
-
-// Source de données du vecteur en format GeoJSON
-const sourceGeoJSON = new VectorSource({
-	url: 'data/pays.geojson',
-	format: new GeoJSON()
-});
-// Déclaration de la couche vectorielle	
-const vecteurGeoJSON = new VectorLayer({
-	source: sourceGeoJSON
-});
-// Déclaration de la carte
-const map = new Map({
-	layers: [vecteurGeoJSON],
-	target: 'map',
-	view: new View({
-		center: [0,0],
-		zoom: 1
-	}),
-});
-// Déclaration de l'interaction
-const interactionModif = new ol.interaction.Modify({
-	source: sourceGeoJSON,
-	//style: styleDessin
-});
-// Ajout de l'interaction
-map.addInteraction(interactionModif);
-
+ // classes nécéssaires pour afficher la carte
+ import 'ol/ol.css';
+ import Map from 'ol/Map';
+ import View from 'ol/View';
+ 
+ // classes pour les vecteurs
+ import VectorLayer from 'ol/layer/Vector';
+ import VectorSource from 'ol/source/Vector';
+ import GeoJSON from 'ol/format/GeoJSON';
+ 
+ // Interaction pour modifier les géométries des entités.
+ import Modify from 'ol/interaction/Modify';
+ 
+ // Source de données du vecteur en format GeoJSON
+ const sourceGeoJSON = new VectorSource({
+	 url: 'data/pays.geojson',
+	 format: new GeoJSON()
+ });
+ // Déclaration de la couche vectorielle	
+ const vecteurGeoJSON = new VectorLayer({
+	 source: sourceGeoJSON
+ });
+ // Déclaration de la carte
+ const map = new Map({
+	 layers: [vecteurGeoJSON],
+	 target: 'map',
+	 view: new View({
+		 center: [0,0],
+		 zoom: 2
+	 }),
+ });
+ // Déclaration de l'interaction
+ const interactionModif = new Modify({
+	 source: sourceGeoJSON,
+	 //style: styleDessin
+ });
+ // Ajout de l'interaction
+ map.addInteraction(interactionModif);
